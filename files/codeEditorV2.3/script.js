48c89eab8a4554d115e925d58a46672fd3dd2782
la-ser <77540680+la-ser@users.noreply.github.com>
2022-03-15 16:43:15 +0100

    Add files via upload

---
+++ files/codeEditorV2.3/script.js
@@ -0,0 +1,16 @@
+function run() {
+    let htmlCode = document.querySelector(".input-field-1 #html-code").value;
+    let cssCode = "<style>" + document.querySelector(".input-field-2 #css-code").value + "</style>";
+    let jsCode = document.querySelector(".input-field-2 #js-code").value;
+    let output = document.querySelector(".output-field #output");
+    //console.log(htmlCode,cssCode,jsCode,output);
+    output.contentDocument.body.innerHTML = htmlCode + cssCode;
+    output.contentWindow.eval(jsCode);
+}
+document.querySelector(".input-field-1 #html-code").addEventListener("keyup", run);
+document.querySelector(".input-field-2 #css-code").addEventListener("keyup", run);
+document.querySelector(".input-field-2 #js-code").addEventListener("keyup", run);
+
+window.onbeforeunload = function () {
+    return "Data will be lost if you leave the page, are you sure?";
+};
